3137591c47889df1df5840d5e3256c7ea69241d0
Daniel Santander <danyvo1d@gmail.com>
2020-11-13 14:26:57 -0600

    chore: configured session_store according to environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # We define what the cookies are going to be structured like
-
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://railsreactapi-1418.heroku.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app",
+  domain: "https://railsreactapi-1418.heroku.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
